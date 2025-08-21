b3f3c6d941d79cefd5cbee144fcd9eb5cd934e59
James Wall <james.wall95@gmail.com>
2025-08-20 20:59:02 -0400

    Adding boilerplate Event class in models.py. will need to be updated with the information that we want

---
+++ src/models.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class Event:
+    title: str
+    venue: str
+    start_time: datetime | None
+    description: str
+    url: str
+    source: str
